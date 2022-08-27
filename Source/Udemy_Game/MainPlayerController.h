4ea5d5a03c650d39090ada028a31152eec1b1c76
Salvatore <78374972+Adamska-01@users.noreply.github.com>
2022-08-27 20:07:02 +0100

    Started creating HUD

---
+++ Source/Udemy_Game/MainPlayerController.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MainPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMY_GAME_API AMainPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
